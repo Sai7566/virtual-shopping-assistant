73fdf043528faa53fdede428b000b69d523495a0
Sai7566 <karrinagasaimanikanta@gmail.com>
2025-04-10 17:36:26 +0530

    Added backend API route

--- backend/api/hello.js
+++ backend/api/hello.js
@@ -1,4 +1,4 @@
+// pages/api/test.js
 export default function handler(req, res) {
-    res.status(200).json({ message: 'Hello from Vercel Backend!' });
-  }
-  
+  res.status(200).json({ message: "Backend test success!" });
+}
